d9adc641f818d1da39f08aadf9612efdc26d5ea3
Nidhi Jain <contact.nidhijain@gmail.com>
2017-03-12 00:03:43 +0530

    Add material design moldule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { EchoBotComponent } from './echo-bot/echo-bot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EchoBotComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
